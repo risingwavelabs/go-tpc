e88eeda1ce2364b85ad668c4694f35844090376d
Chengyou Liu <chengyou.liu0@gmail.com>
2024-03-18 15:18:14 +0800

    remove debug println

--- pkg/util/risingwave.go
+++ pkg/util/risingwave.go
@@ -1,17 +1,14 @@
 package util
 
 import (
-	"fmt"
 	"regexp"
 )
 
 func ConvertToRisingWaveDDL(query string) string {
-	fmt.Println(query)
 	query = regexp.MustCompile("(?i)varchar\\(\\d+\\)").ReplaceAllString(query, "VARCHAR")
 	query = regexp.MustCompile("(?i)numeric\\(.*?\\)").ReplaceAllString(query, "NUMERIC")
 	query = regexp.MustCompile("(?i)decimal\\(.*?\\)").ReplaceAllString(query, "DECIMAL")
 	query = regexp.MustCompile("(?i)char\\(\\d+\\)").ReplaceAllString(query, "VARCHAR")
 	query = regexp.MustCompile("(?i) not null").ReplaceAllString(query, "")
-	fmt.Println(query)
 	return query
 }
